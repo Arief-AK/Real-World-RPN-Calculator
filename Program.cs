4d30ce7b926fc33fea7294aaaf99eccae0e30322
Arief-AK <mariefak22@gmail.com>
2021-05-23 19:19:31 +0200

    Update file

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 
 namespace ReversePolishNotation
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nRPN Calculator");
+            var calc = new RpnCalculator();
+            var hInput = new InputHandle();
+            var hOutput = new OutputHandle();
+            
+            calc.AddOperation(new Addition());
+            calc.AddOperation(new Subtraction());
+            calc.AddOperation(new Multiplication());
+            calc.AddOperation(new Division());
+            calc.AddOperation(new SqRoot());
+            
+            var controller = new Controller(calc, hInput, hOutput);
+            controller.Run();
         }
     }
 }
